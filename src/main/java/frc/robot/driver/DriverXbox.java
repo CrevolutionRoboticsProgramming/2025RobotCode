8388cb6a40906542a51657ab546193ced61fcccd
EdwardL <edward.lei1234@gmail.com>
2025-01-02 23:15:20 -0500

    Inital Stuff For A Great 2025 Season (Errors Around) (KOW)

---
+++ src/main/java/frc/robot/driver/DriverXbox.java
@@ -0,0 +1,143 @@
+package frc.robot.driver;
+
+import java.util.Optional;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.crevolib.util.ExpCurve;
+import frc.crevolib.util.XboxGamepad;
+import frc.robot.Robot;
+import frc.robot.drivetrain.Drivetrain;
+
+public class DriverXbox extends XboxGamepad {
+    private static class Settings {
+        static final int port = 0;
+        static final String name = "driver";
+
+        static final double kTranslationExpVal = 2.0;
+        static final double kRotationExpVal = 1.0;
+        static final double kDeadzone = 0.1;
+    }
+
+    private static DriverXbox mInstance;
+    public static ExpCurve translationStickCurve;
+    private static ExpCurve rotationStickCurve;
+    public boolean autoAim;
+    private double reqAngularVel;
+
+    private DriverXbox() {
+        super(DriverXbox.Settings.name, DriverXbox.Settings.port);
+
+        translationStickCurve = new ExpCurve(DriverXbox.Settings.kTranslationExpVal, 0, 1, DriverXbox.Settings.kDeadzone);
+        rotationStickCurve = new ExpCurve(DriverXbox.Settings.kRotationExpVal, 0, 1, DriverXbox.Settings.kDeadzone);
+    }
+
+    public static DriverXbox getInstance() {
+        if (mInstance == null) {
+            mInstance = new DriverXbox();
+        }
+        return mInstance;
+    }
+
+    @Override
+    public void setupTeleopButtons() {
+        // Drivetrain Commands
+
+
+
+        // Future notice: A = speaker and x = amp wil be amp and speaker shoot on move
+        // controller.a().whileTrue(DrivetrainCommands.shootSpeakerMode(() -> mInstance.getDriveTranslation(), () -> mInstance.getDriveRotation(), true, false));
+        // controller.a().whileFalse(DrivetrainCommands.shootSpeakerMode(() -> mInstance.getDriveTranslation(), () -> mInstance.getDriveRotation(), false, false));
+
+        // controller.x().whileFalse(DrivetrainCommands.shootAmpMode(() -> mInstance.getDriveTranslation(), () -> mInstance.getDriveRotation(), false, true));
+        // controller.x().whileFalse(DrivetrainCommands.shootAmpMode(() -> mInstance.getDriveTranslation(), () -> mInstance.getDriveRotation(), false, false));
+
+
+
+        // controller.L1().whileTrue(new ConditionalCommand(
+        //     DrivetrainCommands.turnToAngle(Rotation2d.fromDegrees(28.51)),
+        //     DrivetrainCommands.turnToAngle(Rotation2d.fromDegrees(-28.51)),
+        //     () -> {
+        //         var alliance = DriverStation.getAlliance();
+        //         if (alliance.isPresent()) {
+        //             return alliance.get() == DriverStation.Alliance.Red;
+        //         }
+
+        //         return false;
+        //     }
+        // ));
+    }
+
+    @Override
+    public void setupDisabledButtons() {}
+
+    @Override
+    public void setupTestButtons() {}
+
+    public Translation2d getDriveTranslation() {
+        final var xComponent = translationStickCurve.calculate(-controller.getLeftX());
+        final var yComponent = translationStickCurve.calculate(-controller.getLeftY());
+        // Components are reversed because field coordinates are opposite of joystick coordinates
+        // System.out.println("Colin Is GAY!!! " + new Translation2d(yComponent, xComponent).toString());
+        return new Translation2d(yComponent, xComponent);
+    }
+
+    public void setDriveRotation(double requestedAngularVel) {
+        reqAngularVel = requestedAngularVel;
+    }
+
+    public double getDriveRotation() {
+        return rotationStickCurve.calculate(-controller.getRightX());
+        // return reqAngularVel;
+        // System.out.println("Is it true??? " + autoAim);
+        // if (true) {
+        //     final Drivetrain drivetrain = Drivetrain.getInstance();
+
+        //     PIDController pidController = new PIDController(1.0, 0.0, 0.1);
+            
+        //     Rotation2d deltaTheta;
+        //     Rotation2d targetAngle;
+        //     Rotation2d kMaxAngularVelocity = Rotation2d.fromDegrees(1.0);
+
+        //     Pose2d goalPose = null;
+            
+
+        //     final var mPoseEstimator = Vision.PoseEstimator.getInstance();
+        //     final var robotPose = mPoseEstimator.getCurrentPose();
+
+        //     Optional<Alliance> ally = DriverStation.getAlliance();
+        //     if (ally.isPresent()) {
+        //         if (ally.get() == Alliance.Blue) {
+        //             goalPose = new Pose2d(new Translation2d(Units.inchesToMeters(-1.5), Units.inchesToMeters(218.42)), new Rotation2d(0));
+        //         }
+        //         if (ally.get() == Alliance.Red) {
+        //             goalPose = new Pose2d(new Translation2d(Units.inchesToMeters(652.73), Units.inchesToMeters(218.42)), new Rotation2d(Units.degreesToRadians(180)));
+        //         }
+        //     }
+        //     final var startingAngle = robotPose.getRotation();
+        //     final var endAngle = goalPose.getTranslation().minus(robotPose.getTranslation()).getAngle().plus(Rotation2d.fromDegrees(180.0));
+        //     deltaTheta = endAngle.minus(startingAngle);
+
+        //     targetAngle = Rotation2d.fromDegrees(drivetrain.getGyroYaw().getDegrees() + deltaTheta.getDegrees());
+
+        //     final var currentAngle = drivetrain.getGyroYaw();
+        //     final var requestedAngularVelocity = Rotation2d.fromDegrees(MathUtil.clamp(
+        //         pidController.calculate(currentAngle.getDegrees(), targetAngle.getDegrees()),
+        //         -kMaxAngularVelocity.getDegrees(),
+        //         kMaxAngularVelocity.getDegrees()
+        //     ));
+
+        //     System.out.println("requested Angular Vel: " + requestedAngularVelocity.getDegrees());
+        //     return requestedAngularVelocity.getDegrees();
+        // } else {
+        //     return rotationStickCurve.calculate(-controller.getRightX());
+        // }
+    }
+}
---
+++ src/main/java/frc/robot/operator/OperatorXbox.java
@@ -0,0 +1,160 @@
+package frc.robot.operator;
+
+import edu.wpi.first.math.estimator.PoseEstimator;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import frc.crevolib.util.ExpCurve;
+import frc.crevolib.util.Gamepad;
+import frc.crevolib.util.XboxGamepad;
+import frc.robot.Robot;
+import frc.robot.commands.RobotCommands;
+import frc.robot.drivetrain.commands.DrivetrainCommands;
+import frc.robot.elevator.Elevator;
+import frc.robot.elevator.commands.ElevatorCommands;
+import frc.robot.elevator.commands.SetPositionElevator;
+import frc.robot.indexer.commands.IndexerCommands;
+import frc.robot.intakepivot.IntakePivot;
+import frc.robot.intakepivot.commands.IntakePivotCommands;
+import frc.robot.intakeroller.commands.IntakeRollerCommands;
+import frc.robot.shooterflywheel.ShooterFlywheel;
+import frc.robot.shooterflywheel.commands.ShooterFlywheelCommands;
+import frc.robot.shooterpivot.ShooterPivot;
+import frc.robot.shooterpivot.commands.SetAngleShooterPivot;
+import frc.robot.shooterpivot.commands.ShooterPivotCommands;
+import frc.robot.vision.Vision;
+
+public class OperatorXbox extends XboxGamepad {
+    private static class Settings {
+        static final int port = 1;
+        static final String name = "operator";
+
+        static final double kDeadzone = 0.1;
+    }
+
+    ExpCurve stickCurve;
+    ExpCurve shooterPivotManualCurve;
+    ExpCurve intakePivotManualCurve;
+    ExpCurve positionTestCurve;
+    ExpCurve elevatorCurve;
+    private static OperatorXbox mInstance;
+
+    ExpCurve shooterManualCurve;
+
+    private OperatorXbox() {
+        super(Settings.name, Settings.port);
+
+        stickCurve = new ExpCurve(1, 0, 1, Settings.kDeadzone);
+        shooterPivotManualCurve = new ExpCurve(1, 0, ShooterPivot.Settings.kMaxAngularVelocity.getRadians()*0.3, Settings.kDeadzone);
+        intakePivotManualCurve = new ExpCurve(1, 0, IntakePivot.Settings.kMaxAngularVelocity.getRadians()*0.5, Settings.kDeadzone);
+        positionTestCurve = new ExpCurve(1, 20, 15, Settings.kDeadzone);
+        elevatorCurve = new ExpCurve(1, 0, Elevator.Settings.kMaxVelocity, Settings.kDeadzone);
+
+        shooterManualCurve = new ExpCurve(1, 0, ShooterFlywheel.Settings.kMaxAngularVelocity.getRadians(), 0.05);
+    }
+
+    public static OperatorXbox getInstance() {
+        if (mInstance == null) {
+            mInstance = new OperatorXbox();
+        }
+        return mInstance;
+    }
+
+    @Override
+    public void setupTeleopButtons() {
+        // Shooter Commands
+        controller.a().whileTrue(RobotCommands.handOffNote());
+
+        // controller.x().whileTrue(RobotCommands.pass());
+        controller.b().whileTrue(RobotCommands.amp());
+
+        controller.x().whileTrue(RobotCommands.shootCleanUp());
+
+        controller.y().whileTrue(new SequentialCommandGroup(
+            IndexerCommands.unJamNote(),
+            IndexerCommands.grabNote())
+        );
+
+        controller.povUp().whileTrue(RobotCommands.primeClimb());
+        controller.povDown().whileTrue(RobotCommands.climb());
+
+        // controller.triangle().whileTrue(new SequentialCommandGroup(
+        //     IndexerCommands.unJamNote(),
+        //     RobotCommands.primeSpeaker(SetAngleShooterPivot.Preset.kShooterNear))
+        // );
+
+        controller.rightBumper().whileTrue(IndexerCommands.setOutput(() -> 1.0));
+        controller.leftTrigger().and(controller.rightBumper()).whileTrue(RobotCommands.shootCleanUp());
+
+        controller.rightStick().onTrue(RobotCommands.zero());
+        controller.leftStick().whileTrue(ShooterFlywheelCommands.setAngularVelocity(
+            () -> ShooterFlywheel.Settings.kMaxAngularVelocity.times(0.80),
+            () -> ShooterFlywheel.Settings.kMaxAngularVelocity.times(1.0)
+        ));
+
+        // Trap Command
+        // controller.leftBumper().whileTrue(IndexerCommands.setOutput(() -> -1.0));
+        // controller.leftBumper().whileTrue(ShooterFlywheelCommands.setAngularVelocity(() -> Rotation2d.fromRotations(75)));
+
+        // Elevator Manual Override
+        // controller.rightTrigger().whileTrue( Commands.sequence(
+        //     // Ensure manual override doesn't overextend height extension
+        //     new ConditionalCommand(
+        //         ShooterPivotCommands.setSt(SetAngleShooterPivot.Preset.kClimb),
+        //         Commands.none(),
+        //         () -> elevatorCurve.calculate(-controller.getRightY()) > 0 && ShooterPivot.getInstance().getAngle().getDegrees() > SetAngleShooterPivot.Preset.kClimb.getDegrees()
+        //     ),
+        //     ElevatorCommands.setVelocity(() -> elevatorCurve.calculate(-controller.getRightY()))
+        // ));
+
+        // controller.rightTrigger().whileTrue(
+        //     ElevatorCommands.setVelocity(() -> elevatorCurve.calculate(-controller.getRightY()))
+        // );
+
+                // ONlY For Testing
+        // controller.square().whileTrue(ShooterFlywheelCommands.setAngularVelocity(
+        //     () -> ShooterFlywheel.Settings.kMaxAngularVelocity.times(0.85),
+        //     () -> ShooterFlywheel.Settings.kMaxAngularVelocity.times(0.6)
+        // ));
+
+
+        //Intake Pivot Mnaual Override
+        // controller.leftTrigger().whileTrue(
+        //     IntakePivotCommands.setAngularVelocity(() -> Rotation2d.fromRadians(
+        //             intakePivotManualCurve.calculate(controller.getLeftX())),
+        //         false)
+        // );
+
+        // //Shooter Pivot Manual Override
+        controller.leftTrigger().whileTrue(
+            ShooterPivotCommands.setAngularVelocity(() -> Rotation2d.fromRadians(
+                shooterPivotManualCurve.calculate(controller.getRightX())),
+                false)
+        );
+    }
+
+    @Override
+    public void setupDisabledButtons() {
+
+    }
+
+    @Override
+    public void setupTestButtons() {
+    }
+
+
+
+    public Translation2d getDriveTranslation() {
+        final var xComponent = stickCurve.calculate(controller.getLeftX());
+        final var yComponent = stickCurve.calculate(controller.getLeftY());
+        // Components are reversed because field coordinates are opposite of joystick coordinates
+        return new Translation2d(yComponent, xComponent);
+    }
+
+    public double getDriveRotation() {
+        return -stickCurve.calculate(controller.getRightX());
+    }
+}
