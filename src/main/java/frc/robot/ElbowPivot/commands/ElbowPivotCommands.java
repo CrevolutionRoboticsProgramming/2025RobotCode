71b07b88caf1f919acd7333797cc48f0f0099ef2
EdwardL <edward.lei1234@gmail.com>
2025-02-12 19:04:48 -0500

    Created command files

---
+++ src/main/java/frc/robot/ElbowPivot/commands/ElbowPivotCommands.java
@@ -0,0 +1,5 @@
+package frc.robot.ElbowPivot.commands;
+
+public class ElbowPivotCommands {
+    
+}
---
+++ src/main/java/frc/robot/WristPivot/commands/WristPivotCommands.java
@@ -0,0 +1,5 @@
+package frc.robot.WristPivot.commands;
+
+public class WristPivotCommands {
+    
+}
