075b8ed3b544089864fa6086a75afa91dbcfdbca
Pix_XL <82770059+PIXLwastaken@users.noreply.github.com>
2025-02-15 11:59:57 -0500

    New way to estimate pose w multicam
    7028 is goated and im not

--- src/main/java/frc/robot/vision/Vision.java
+++ src/main/java/frc/robot/vision/Vision.java
@@ -1,6 +1,8 @@
 package frc.robot.vision;
 
-import static frc.robot.vision.VisionConfig.*;
+import static frc.robot.vision.VisionConfig.multiTagStdDevs;
+import static frc.robot.vision.VisionConfig.singleTagStdDevs;
+import static frc.robot.vision.VisionConfig.tagLayout;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,91 +14,91 @@ import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Vision extends SubsystemBase {
+/*
+ * This code is a modified version of Photonvision's own example and 7028's vision code:
+ * https://github.com/PhotonVision/photonvision/blob/main/photonlib-java-examples/poseest
+ * https://github.com/STMARobotics/frc-7028-2025
+ */
 
+public class Vision {
+
+    private final PhotonCamera cam;
+    private final PhotonPoseEstimator photonEstimator;
+    private Matrix<N3, N1> curStdDevs;
+
+    //Construct Vision Instance
+    public Vision(String cameraName, Transform3d camTransform){
+        cam = new PhotonCamera(cameraName);
+
+        photonEstimator = new PhotonPoseEstimator(
+            tagLayout, 
+            PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, 
+            camTransform);
+        photonEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+    }
 
     /**
-     * This code is a modified version of the example found in:
-     * https://github.com/PhotonVision/photonvision/blob/main/photonlib-java-examples/poseest
-     * 
-     * The modifications (should) allow it to run pose estimators using multiple cameras and detect objects
-     */
-
-    public class VisionEstimator {
-        private final PhotonCamera photonCamera;
-        private final Transform3d robotToCam;
-        private final PhotonPoseEstimator poseEstimator;
-        private Matrix<N3, N1> stdDevs;
+    * The latest estimated robot pose on the field from vision data. This may be empty. This should
+    * only be called once per loop.
+    *
+    * <p>
+    * Also includes updates for the standard deviations, which can (optionally) be retrieved with
+    * {@link getEstimationStdDevs}
+    *
+    * @return An {@link EstimatedRobotPose} with an estimated pose, estimate timestamp, and targets
+    *         used for estimation.
+    */
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose() {
+        Optional<EstimatedRobotPose> visionEst = Optional.empty();
+        for (var change : cam.getAllUnreadResults()) {
+          visionEst = photonEstimator.update(change);
+          updateEstimationStdDevs(visionEst, change.getTargets());
+        }
+        return visionEst;
+    }
+    /**
+    * Calculates new standard deviations This algorithm is a heuristic that creates dynamic standard
+    * deviations based on number of tags, estimation strategy, and distance from the tags.
+    *
+    * @param estimatedPose The estimated pose to guess standard deviations for.
+    * @param targets All targets in this camera frame
+    */
+    private void updateEstimationStdDevs(Optional<EstimatedRobotPose> estimatedPose, List<PhotonTrackedTarget> targets){
+        if (estimatedPose.isEmpty()){
+            //No pose input. Default to single-tag std devs
+            curStdDevs = singleTagStdDevs;
         
+        } else {
+            //Pose present, start Heuristic
+            var estStdDevs = singleTagStdDevs;
+            int numTags = 0;
+            double avgDist = 0;
 
-        public VisionEstimator(PhotonCamera camName, Transform3d camLocation) {
-            this.photonCamera = camName;
-            this.robotToCam = camLocation;
-
-            // declares a new photon pose estimator using the given parameters
-            // also sets the fall back strategy to LOWEST_AMBIGUITY in case MULTI_TAG_PNP_ON_COPROCESSOR fails
-            poseEstimator = new PhotonPoseEstimator(
-                tagLayout, 
-                PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, 
-                camLocation);
-            poseEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+            //Precalculation - see how many tags we found, and calculate an average-distance metric
+            for (var tgt : targets) {
+                var tagPose = photonEstimator.getFieldTags().getTagPose(tgt.getFiducialId());
+                if (tagPose.isEmpty()){
+                    continue;
+                }
+                numTags++;
+                avgDist += tagPose.get()
+                    .toPose2d()
+                    .getTranslation()
+                    .getDistance(estimatedPose.get().estimatedPose.toPose2d().getTranslation());
             }
 
-            /**
-             * The latest estimated robot pose on the field from vision data. This may be empty. This should
-             * only be called once per loop.
-             *
-             * <p>Also includes updates for the standard deviations, which can (optionally) be retrieved with
-             * {@link getEstimationStdDevs}
-             *
-             * @return An {@link EstimatedRobotPose} with an estimated pose, estimate timestamp, and targets
-             *     used for estimation.
-             */
-            public Optional<EstimatedRobotPose> getEstimatedGlobalPose() {
-                Optional<EstimatedRobotPose> visionEst = Optional.empty();
-                for (var change : photonCamera.getAllUnreadResults()) {
-                    visionEst = poseEstimator.update(change);
-                    updateEstimationStdDevs(visionEst, change.getTargets());
-                }
-                return visionEst;
-            }
-            
-            /**
-             * Calculates new standard deviations This algorithm is a heuristic that creates dynamic standard
-             * deviations based on number of tags, estimation strategy, and distance from the tags.
-             *
-             * @param estimatedPose The estimated pose to guess standard deviations for.
-             * @param targets All targets in this camera frame
-             */
-            private void updateEstimationStdDevs(
-                    Optional<EstimatedRobotPose> estimatedPose, List<PhotonTrackedTarget> targets) {
-                if (estimatedPose.isEmpty()) {
-                    // No pose input. Default to single-tag std devs
-                    stdDevs = singleTagStdDevs;
-
-                } else {
-                    // Pose present. Start running Heuristic
-                    var estStdDevs = singleTagStdDevs;
-                    int numTags = 0;
-                    double avgDist = 0;
-
-                    // Precalculation - see how many tags we found, and calculate an average-distance metric
-                    for (var tgt : targets) {
-                        var tagPose = poseEstimator.getFieldTags().getTagPose(tgt.getFiducialId());
-                        if (tagPose.isEmpty()) continue;
-                        numTags++;
-                        avgDist += tagPose.get().toPose2d().getTranslation().getDistance(
-                                    estimatedPose.get().estimatedPose.toPose2d().getTranslation()
-                                    );
-                    }
-                    
-                }
-
+            if (numTags == 0){
+                //No tags visible. Default to single-tag std devs
+                curStdDevs = singleTagStdDevs;
+            } else {
+                //One or more tags visible, run the full heuristic
             }
         }
     }
+    
+}
--- src/main/java/frc/robot/vision/VisionConfig.java
+++ src/main/java/frc/robot/vision/VisionConfig.java
@@ -16,17 +16,16 @@ import edu.wpi.first.math.util.Units;
 public class VisionConfig {
     
     // Creates camera names; ensure these all match with the correct camera on the Photonvison Dashboard
-    // public static final String centerPoseCamName = "Center_Pose_Cam";
-    // public static final String leftPoseCamName = "Left_Pose_Cam";
-    // public static final String rightPoseCamName = "Right_Pose_Cam";
-    // public static final String driveCamName = "Drive_Cam";
+    public static final String[] camNames = new String[] {"Center_Cam", "Left_Cam", "Right_Cam", "Drive_Cam"};
 
-
-    // Creates all Photoncameras that will be used in pose estimator and commands
-    public static final PhotonCamera centerCam = new PhotonCamera("Center_Pose_Cam");
-    public static final PhotonCamera leftCam = new PhotonCamera("Left_Pose_Cam");
-    public static final PhotonCamera rightCam = new PhotonCamera("Right_Pose_Cam");
-    public static final PhotonCamera driveCam = new PhotonCamera("Drive_Cam");
+    //Camera Positions
+    // TODO: config camera transforms
+    public static final Transform3d[] robotToCamTransforms = new Transform3d[] {
+        new Transform3d(new Translation3d(), new Rotation3d()),
+        new Transform3d(new Translation3d(), new Rotation3d()),
+        new Transform3d(new Translation3d(), new Rotation3d()),
+        new Transform3d(new Translation3d(), new Rotation3d())
+    }; 
 
     // Creates field layout for AprilTags
     public static final AprilTagFieldLayout tagLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
@@ -36,28 +35,5 @@ public class VisionConfig {
     public static final Matrix<N3, N1> singleTagStdDevs = VecBuilder.fill(4, 4, 8); // TODO: example values, change when testing
     public static final Matrix<N3, N1> multiTagStdDevs = VecBuilder.fill(0.5, 0.5, 1); //TODO: change values when testing
 
-    // TODO: config 
-    public static final Transform3d robotToCenterCam = 
-        new Transform3d(
-            new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0)),
-            new Rotation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0)) 
-        );
     
-    public static final Transform3d robotToLeftCam = 
-        new Transform3d(
-            new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0)),
-            new Rotation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0)) 
-        );
-
-    public static final Transform3d robotToRightCam = 
-        new Transform3d(
-            new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0)),
-            new Rotation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0)) 
-        );
-    
-    public static final Transform3d robotToDriveCam =
-        new Transform3d(
-            new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0)),
-            new Rotation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0))
-        );   
 }
