899967763b5c763fbe50e8982cc8e9e69b4cc528
SatchitK <79611051+SatchitK@users.noreply.github.com>
2025-03-08 22:59:04 -0500

    added a Left Starting 3 Coral Auton Path

--- src/main/java/frc/robot/rushinator/RushinatorWrist.java
+++ src/main/java/frc/robot/rushinator/RushinatorWrist.java
@@ -3,5 +3,13 @@ package frc.robot.rushinator;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class RushinatorWrist extends SubsystemBase {
-    
+
+    public RushinatorWrist() {  
+        
+    }
+
+    @Override
+    public void periodic() {
+
+    }
 }
