7fe0da4c3147940a22c72186baf2b0a1b2bf9863
SatchitK <79611051+SatchitK@users.noreply.github.com>
2025-03-16 02:05:25 -0400

    added Joey poses - sorry for the messy code

--- src/main/java/frc/robot/vision/commands/LineupCommand.java
+++ src/main/java/frc/robot/vision/commands/LineupCommand.java
@@ -6,14 +6,23 @@ import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecondPerSecond;
 
+import java.util.Optional;
+
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveModule.SteerRequestType;
+import com.ctre.phoenix6.sim.ChassisReference;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.google.flatbuffers.Constants;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import frc.robot.vision.VisionConfig;
@@ -84,6 +93,112 @@ public class LineupCommand extends Command {
     @Override
     public void end(boolean interrupted) {
         
+    }
+
+    private Pose2d getTargetReefPose(boolean left){ 
+        ChassisSpeeds currentSpeeds = CommandSwerveDrivetrain.getInstance().getState().Speeds;
+        double X = currentPose.getTranslation().getX() * currentSpeeds.vxMetersPerSecond * 0.1;
+        double Y = currentPose.getTranslation().getY() * currentSpeeds.vyMetersPerSecond * 0.1;
+
+        double intercept = Math.tan(Units.degreesToRadians(30))*4.5;
+        double interceptRed = Math.tan(Units.degreesToRadians(30))*13;
+        double slope = Math.tan(Units.degreesToRadians(30));
+
+        var currAlliance = DriverStation.getAlliance();
+
+        //TODO: map currentPose to a Reef Zone and then finding closest lineup based on heading
+
+        if(currAlliance.get() == Alliance.Blue) {
+
+            if (Y <= -X*slope + intercept+4 && Y >= X*slope - intercept+4 && left && X <= 4.5) {
+            System.out.println("Ablue");
+            return VisionConfig.AlignmentConfig.Ablue;
+            } else if (Y <= -X*slope + intercept+4 && Y >= X*slope - intercept+4 && !left && X <= 4.5) {
+            System.out.println("Bblue");
+            return VisionConfig.AlignmentConfig.Bblue;
+            } else if (Y <= X*slope - intercept+4 && X <= 4.5 && left) {
+            System.out.println("Cblue");
+            return VisionConfig.AlignmentConfig.Cblue;
+            } else if (Y <= X*slope - intercept+4 && X <= 4.5 && !left) {
+            System.out.println("Dblue");
+            return VisionConfig.AlignmentConfig.Dblue;
+            } else if (Y <= -X*slope + intercept+4 && X >= 4.5 && !left) {
+            System.out.println("Eblue");
+            return VisionConfig.AlignmentConfig.Eblue;
+            } else if (Y <= -X*slope + intercept+4 && X >= 4.5 && left) {
+            System.out.println("Fblue");
+            return VisionConfig.AlignmentConfig.Fblue;
+            } else if (Y >= -X*slope + intercept+4 && Y <= X*slope - intercept+4 && !left && X >= 4.5) {
+            System.out.println("Gblue");
+            return VisionConfig.AlignmentConfig.Gblue;
+            } else if (Y >= -X*slope + intercept+4 && Y <= X*slope - intercept+4 && left && X >= 4.5) {
+            System.out.println("Hblue");
+            return VisionConfig.AlignmentConfig.Hblue;
+            } else if (Y >= X*slope - intercept+4 && X >= 4.5 && !left) {
+            System.out.println("Iblue");
+            return VisionConfig.AlignmentConfig.Iblue;
+            } else if (Y >= X*slope - intercept+4 &&  X >= 4.5 && left) {
+            System.out.println("Jblue");
+            return VisionConfig.AlignmentConfig.Jblue;
+            } else if (Y >= -X*slope + intercept+4 && X <= 4.5 && left) {
+            System.out.println("Kblue");
+            return VisionConfig.AlignmentConfig.Kblue;
+            } else if (Y >= -X*slope + intercept+4 && X <= 4.5 && !left) {
+            System.out.println("Lblue");
+            return VisionConfig.AlignmentConfig.Lblue;
+            } else {
+            System.out.println("error");
+            return VisionConfig.AlignmentConfig.Error;
+            }
+        }
+        else if(currAlliance.get() == Alliance.Red) {
+            if (Y <= -X*slope + interceptRed+4 && Y >= X*slope - interceptRed+4 && !left && X <= 13.0) {
+                System.out.println("Gred");
+                return VisionConfig.AlignmentConfig.Gred;
+                } else if (Y <= -X*slope + interceptRed+4 && Y >= X*slope - interceptRed+4 && left && X <= 13.0) {
+                System.out.println("Hred");
+                return VisionConfig.AlignmentConfig.Hred;
+                } else if (Y <= X*slope - interceptRed+4 && X <= 13.0 && !left) {
+                System.out.println("Ired");
+                return VisionConfig.AlignmentConfig.Ired;
+                } else if (Y <= X*slope - interceptRed+4 && X <= 13.0 && left) {
+                System.out.println("Jred");
+                return VisionConfig.AlignmentConfig.Jred;
+                } else if (Y <= -X*slope + interceptRed+4 && X >= 13.0 && left) {
+                System.out.println("Kred");
+                return VisionConfig.AlignmentConfig.Kred;
+                } else if (Y <= -X*slope + interceptRed+4 && X >= 13.0 && !left) {
+                System.out.println("Lred");
+                return VisionConfig.AlignmentConfig.Lred;
+                } else if (Y >= -X*slope + interceptRed+4 && Y <= X*slope - interceptRed+4 && left && X >= 13.0) {
+                System.out.println("Ared");
+                return VisionConfig.AlignmentConfig.Ared;
+                } else if (Y >= -X*slope + interceptRed+4 && Y <= X*slope - interceptRed+4 && !left && X >= 13.0) {
+                System.out.println("Bred");
+                return VisionConfig.AlignmentConfig.Bred;
+                } else if (Y >= X*slope - interceptRed+4 && X >= 13.0 && left) {
+                System.out.println("Cred");
+                return VisionConfig.AlignmentConfig.Cred;
+                } else if (Y >= X*slope - interceptRed+4 &&  X >= 13.0 && !left) {
+                System.out.println("Dred");
+                return VisionConfig.AlignmentConfig.Dred;
+                } else if (Y >= -X*slope + interceptRed+4 && X <= 13.0 && !left) {
+                System.out.println("Ered");
+                return VisionConfig.AlignmentConfig.Ered;
+                } else if (Y >= -X*slope + interceptRed+4 && X <= 13.0 && left) {
+                System.out.println("Fred");
+                return VisionConfig.AlignmentConfig.Fred;
+                } else {
+                System.out.println("error");
+                return VisionConfig.AlignmentConfig.Error;
+                }
+        }
+        else {
+            System.out.println("Error: Invalid Alliance (Lineup Command)");
+        }
+
+        System.out.println("Error: no suitable target pose found (Lineup Command)");
+        return new Pose2d(new Translation2d(0,0), Rotation2d.fromDegrees(0));
     }
 }
 
