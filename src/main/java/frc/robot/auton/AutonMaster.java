f118697aa6b40c9c31b18e764c05ee08dd67a37e
SatchitK <79611051+SatchitK@users.noreply.github.com>
2025-03-22 00:31:03 -0400

    fuck this. autons are done (untested). let's pull a 2023

--- src/main/java/frc/robot/auton/AutonMaster.java
+++ src/main/java/frc/robot/auton/AutonMaster.java
@@ -2,6 +2,7 @@ package frc.robot.auton;
 
 import static edu.wpi.first.units.Units.Rotation;
 
+import com.fasterxml.jackson.databind.util.Named;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.config.PIDConstants;
@@ -22,8 +23,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.commands.RobotCommands;
 import frc.robot.driver.DriverXbox;
 import frc.robot.drivetrain.CommandSwerveDrivetrain;
+import frc.robot.elevator.ElevatorSubsystem;
+import frc.robot.rushinator.RushinatorPivot;
+import frc.robot.rushinator.RushinatorWrist;
 
 /* MASTER AUTON CLASS */
 public class AutonMaster {
@@ -67,8 +72,12 @@ public class AutonMaster {
         configurePathPlannerLogging();
         
         //Add Auton Options Here
-        autonChooser.addOption("TestPath", AutoBuilder.buildAuto("TestAuto"));
-        autonChooser.addOption("LeftStart3PieceCoralFeed", AutoBuilder.buildAuto("L-3C-F"));
+        // autonChooser.addOption("TestPath", AutoBuilder.buildAuto("TestAuto"));
+        // autonChooser.addOption("LeftStart3PieceCoralFeed", AutoBuilder.buildAuto("L-3C-F"));
+        autonChooser.addOption("RightStart3PieceCoralFeed", AutoBuilder.buildAuto("Right_3.5_FDC"));
+        autonChooser.addOption("LeftStart3PieceCoralFeed", AutoBuilder.buildAuto("Left_3.5_IKL"));
+        autonChooser.addOption("MidStart1PieceCoral", AutoBuilder.buildAuto("Mid_1AL_G"));
+        
     }
     
 
@@ -80,7 +89,11 @@ public class AutonMaster {
     }
 
     public void configureNamedCommands() {
-       
+        NamedCommands.registerCommand("AutonScoreL1", RobotCommands.scoreCoralAutonL1());
+        NamedCommands.registerCommand("AutonScoreL2", RobotCommands.scoreCoralAutonL2());
+        NamedCommands.registerCommand("AutonScoreL3", RobotCommands.scoreCoralAutonL3());
+        NamedCommands.registerCommand("AutonScoreL4", RobotCommands.scoreCoralAutonL4());
+        NamedCommands.registerCommand("HPPickup", RobotCommands.coralPrime(RushinatorPivot.State.kHPIntake, ElevatorSubsystem.State.kZero, RushinatorWrist.State.kHPMid));
     }
 
     public Command getTestPathFindingCommand() {
